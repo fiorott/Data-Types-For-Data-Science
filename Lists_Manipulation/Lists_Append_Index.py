9e0a411150b049ac2d1694743665d3a748965f7a
fiorott <fiorott@gmail.com>
2020-11-12 11:28:45 -0300

    Adding loops and sorting

---
+++ Lists_Manipulation/Lists_Append_Index.py
@@ -0,0 +1,18 @@
+# Create a list containing the names: baby_names
+baby_names = ['Ximena', 'Aliza', 'Ayden', 'Calvin']
+
+# Extend baby_names with 'Rowen' and 'Sandeep'
+ext = ['Rowen','Sandeep']
+baby_names.extend(ext)
+
+# Print baby_names
+print(baby_names)
+
+# Find the position of 'Aliza': position
+position = baby_names.index('Aliza')
+
+# Remove 'Aliza' from baby_names
+pop_name = baby_names.pop(position)
+
+# Print baby_names
+print(baby_names)
